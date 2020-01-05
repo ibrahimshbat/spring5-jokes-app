a6a74686aa7efc235fcf2525ed1535483b920faa
Ibrahim Elsanosi <ibrahimsabat@yahoo.com>
2020-01-05 00:35:42 +0000

    Add commits

--- src/main/java/guru/springframework/joke/JokeappApplication.java
+++ src/main/java/guru/springframework/joke/JokeappApplication.java
@@ -2,8 +2,13 @@ package guru.springframework.joke;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
+/**
+ * This the main class
+ */
 @SpringBootApplication
+@ImportResource("classpath:chuck-config.xml")
 public class JokeappApplication {
 
     public static void main(String[] args) {
